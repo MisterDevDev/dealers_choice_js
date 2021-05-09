a0aaa61d1ee7f4aa78430f4908d5db022ff34c2f
djb91c <djb91c@gmail.com>
2021-05-09 18:12:11 -0400

    first draft

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const details = require('./details')
 
 const app = express();
+const PORT = 1337;
 
 app.use(express.static('public'));
 
@@ -9,7 +10,45 @@ app.get('/details/:id', (req, res) => {
     const id = req.params.id;
     const robo = details.find(id);
     res.send(
-        robo
+        `<!DOCTYPE html>
+        <html>
+            <head>
+                <link rel='stylesheet' href='/style.css' />
+            </head>
+            <body>
+                <table class='detail-sheet'>
+                    <tr>
+                        <th>${robo.name}'s Detail Sheet</th>
+                    </tr>
+                    <tr>
+                        <td>ID #</td>
+                        <td>${robo.id}</td>
+                    </tr>
+                    <tr>
+                        <td>Fullname</td>
+                        <td>${robo.fullname}</td>
+                    </tr>                    
+                    <tr>
+                        <td>Directive</td>
+                        <td>${robo.directive}</td>
+                    </tr>                    
+                    <tr>
+                        <td>Main Weapon</td>
+                        <td>${robo.weapon}</td>
+                    </tr>
+                    <tr>
+                        <td>Movie</td>
+                        <td>${robo.movie}</td>
+                    </tr>
+                    <tr>
+                        <td>Voice Actor</td>
+                        <td>${robo.actor}
+                    </tr>
+                </table>
+                <div class = 'return-button'>
+                    <h3><a href="http://localhost:${PORT}">Home Page</a></h3>
+                </div>
+            </body>`
     )
 })
 
@@ -20,22 +59,22 @@ app.get('/', (req, res) => {
     res.send(
         `<!DOCTYPE html>
         <html>
-        <head>
-            <link rel='stylesheet' href='/style.css' />
-        </head>
-         <body>
-            <div class='container'>
-                <ul class='robot-list'>
-                    ${robos.map(robo => `
-                    <li><a href="http://localhost:1337/details/${robo.id}">${robo.name}</a></li>`)}
-                </ul>
-            </div>
-         </body>
+            <head>
+                <link rel='stylesheet' href='/style.css' />
+            </head>
+            <body>
+                <h1>Disney Robots</h1>
+                <div class='container'>
+                    <ul class='robot-list'>
+                        ${robos.map(robo => `
+                        <li><a href="http://localhost:${PORT}/details/${robo.id}">${robo.name}</a></li>`)}
+                    </ul>
+                </div>
+            </body>
         </html>`
     )
 });
 
-const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App is listening on Port ${PORT}`)
--- details.js
+++ details.js
@@ -1,16 +1,15 @@
 data = [
-    { id: 1, name: 'WALL-E', fullname: 'Waste Allocation Load Lifter Earth-class', movie: 'WALL-E'},
-    { id: 2, name: 'EVE', fullname: 'Extraterrestrial Vegetation Evaluator', movie: 'WALL-E'},
-    { id: 3, name: 'AUTO', fullname: 'Autopilot', movie: 'WALL-E'},
-    { id: 4, name: 'Baymax', fullname: 'Monster Baymax', movie: 'Big Hero 6'},
-    { id: 5, name: 'B.E.N', fullname: 'BioElectronic Navigator', movie: 'Treasure Planet'},
-    { id: 6, name: 'R2D2', fullname: 'Second Generation Robotic Droid Series-2', movie: 'Star Wars'},
-    { id: 7, name: 'C3PO', fullname: 'Commercial Crew & Cargo Program Office', movie: 'Star Wars'},
-    { id: 8, name: 'J.A.R.V.I.S', fullname: 'Just A Rather Very Intelligent System', movie: 'Avengers: Age of Ultron'}
+    { id: 1, name: 'WALL-E', fullname: 'Waste Allocation Load Lifter Earth-class', directive: 'waste collection', weapon: 'small trash compactor', movie: 'WALL-E', actor: 'Ben Burtt'},
+    { id: 2, name: 'EVE', fullname: 'Extraterrestrial Vegetation Evaluator', directive: 'vegetable reconnaissance', weapon: 'photon cannon', movie: 'WALL-E', actor: 'Elissa Knight'},
+    { id: 3, name: 'AUTO', fullname: 'Autopilot', directive: 'Prevent Recolonization', weapon: 'his wheel shaped body', movie: 'WALL-E', actor: 'Macintalk'},
+    { id: 4, name: 'Baymax', fullname: 'Monster Baymax', directive: 'personal healthcare companion', weapon: 'large belly', movie: 'Big Hero 6', actor: 'Scott Adsit'},
+    { id: 5, name: 'B.E.N', fullname: 'Bio Electronic Navigator', directive: 'space pirate navigator', weapon: 'shipmate telescope', movie: 'Treasure Planet', actor: 'Martin Short'},
+    { id: 6, name: 'R2D2', fullname: 'Second Generation Robotic Droid Series-2', directive: 'astro-navigation', weapon: 'mechanic tools', movie: 'Star Wars', actor: 'Ben Burtt'},
+    { id: 7, name: 'C3PO', fullname: 'Commercial Crew & Cargo Program Office', directive: 'protocol droid',weapon: 'harsh words', movie: 'Star Wars', actor: 'Anthony Daniels'},
+    { id: 8, name: 'J.A.R.V.I.S', fullname: 'Just A Rather Very Intelligent System' ,directive: 'natural language user interface', weapon: 'STARK Inc. Arsenal', movie: 'Avengers: Age of Ultron', actor: 'Paul Bettany'}
 ];
 
 const list = () => {
-    console.log(data)
     return [...data]
 };
 
